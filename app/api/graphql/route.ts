57762edd6e3d6395fef1e16c84e7a9f32133c3b1
Scott Moss <willscottmoss@gmail.com>
2024-05-05 17:18:18 -0500

    init

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,46 +1,9 @@
-import { startServerAndCreateNextHandler } from '@as-integrations/next'
-import { ApolloServer } from '@apollo/server'
-import {
-  ApolloServerPluginLandingPageLocalDefault,
-  ApolloServerPluginLandingPageProductionDefault,
-} from '@apollo/server/plugin/landingPage/default'
-import { NextRequest } from 'next/server'
-import typeDefs from './schema'
-import resolvers from './resolvers'
-import { getUserFromToken } from '@/utils/auth'
-
-let plugins = []
-if (process.env.NODE_ENV === 'production') {
-  plugins = [
-    ApolloServerPluginLandingPageProductionDefault({
-      embed: true,
-      graphRef: 'myGraph@prod',
-    }),
-  ]
-} else {
-  plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
-}
-
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  plugins,
-})
-
-const handler = startServerAndCreateNextHandler<NextRequest>(server, {
-  context: async (req) => {
-    const user = await getUserFromToken(req.headers.get('authorization') ?? '')
-    return {
-      req,
-      user,
-    }
-  },
-})
+import { NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  return handler(request)
+  return NextResponse.json({ data: {} })
 }
 
 export async function POST(request: NextRequest) {
-  return handler(request)
+  return NextResponse.json({ data: {} })
 }
