c7beafadbc7b7c242708de6d90b983f1d66c6456
Scott Moss <willscottmoss@gmail.com>
2024-05-07 13:10:17 -0500

    init

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,9 +1,37 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
+import { ApolloServer } from '@apollo/server'
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default'
+import { schema } from './schema'
+import { resolvers } from './resolvers'
+
+let plugins = []
+if (process.env.NODE_ENV === 'production') {
+  plugins = [
+    ApolloServerPluginLandingPageProductionDefault({
+      embed: true,
+      graphRef: 'myGraph@prod',
+    }),
+  ]
+} else {
+  plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+}
+
+const server = new ApolloServer({
+  resolvers,
+  plugins,
+  typeDefs: schema,
+})
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {})
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ data: {} })
+  return handler(request)
 }
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json({ data: {} })
+  return handler(request)
 }
---
+++ app/api/graphql/schema.ts
@@ -0,0 +1,5 @@
+export const schema = `#graphql
+  type Query {
+    me: String
+  }
+`
