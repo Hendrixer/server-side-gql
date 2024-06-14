6393d556a33e9391051f9b2dd51ea39aa9e5624f
Leah Brenny <66805316+leahbrenny@users.noreply.github.com>
2024-06-14 11:03:32 -0500

    removed extra code from schema

--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,23 +1,5 @@
 const schema = `#graphql
 
-  type Animal {
-    species: String!
-    name: String!
-  }
-
-  type Person {
-    name: String!
-    id: ID!
-    pets: [Animal]!
-  }
-
-  union SearchType = Animal | Person
-
-  type Query {
-    me: String!
-    people: [Person!]!
-    search: [SearchType]!
-  }
 `
 
 export default schema
