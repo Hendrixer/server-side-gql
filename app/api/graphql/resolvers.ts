81939b62dcfe96243435c3e5e4744b124183508f
Leah Brenny <66805316+leahbrenny@users.noreply.github.com>
2024-06-14 15:45:21 -0500

    removed extra code from resolvers.ts

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,35 +1,3 @@
-const resolvers = {
-  SearchType: {
-    __resolveType: (obj) => {
-      if (obj.species) {
-        return 'Animal'
-      }
-      return 'Person'
-    },
-  },
-
-  Person: {
-    name: (person) => {
-      return 22
-    },
-    pets: (person) => {
-      return [{ species: 'cat', name: 'darrylk' }]
-    },
-  },
-  Query: {
-    search: () => {
-      return [
-        { name: 'Scott', id: 'asjflaskfj' },
-        { name: 'Darryl', species: 'Big cat' },
-      ]
-    },
-    me: () => {
-      return 'me'
-    },
-    people: () => {
-      return [{ id: 'sausflsdkj', name: 'henry', pets: [1] }]
-    },
-  },
-}
+const resolvers = {}
 
 export default resolvers
